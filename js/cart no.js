eb9ff8299a97ec5cf9a51850a03f068b9e5d171b
sidd19898 <92017039+sidd19898@users.noreply.github.com>
2024-09-17 16:55:59 +0530

    Initial commit

---
+++ js/cart no.js
@@ -0,0 +1,49 @@
+// Select total count
+const totalCount = document.getElementById("total-count");
+
+// Variable to track count
+var count = 0;
+
+// Display initial count value
+totalCount.innerHTML = count;
+
+
+
+function incrementValue()
+{
+   // Function to increment count
+
+    count++;
+    totalCount.innerHTML = count;
+  
+};
+
+function pRemove()
+{
+   // Function to increment count
+
+    count--;
+    totalCount.innerHTML = count;
+  
+};
+
+// JavaScript code
+function searchP() {
+	let input = document.getElementById('search-box').value
+	input=input.toLowerCase();
+	let x = document.getElementsByClassName('xiaomi');
+	
+	
+}
+
+
+function pCart() 
+{
+    const totalCount = document.getElementById("total-count");
+
+// Variable to track count
+var count = 0;
+
+// Display initial count value
+totalCount.innerHTML = count;
+}
